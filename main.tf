5cacf5b37ecad107fba1efc996a28cc07d957c3b
sasidharkollimarla <sasidharkollimarla@outlook.com>
2020-03-04 15:19:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_api_gateway_rest_api" "example" {
 }
 
 
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "proxy-sasi" {
   rest_api_id = aws_api_gateway_rest_api.example.id
   parent_id   = aws_api_gateway_rest_api.example.root_resource_id
   path_part   = "{proxy+}"
